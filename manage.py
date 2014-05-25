1287577416982432b9d18cbce3fa21e760e6b831
Justus <justus@abi007.info>
2014-05-25 12:25:01 +0200

    The retardation is high with that one.

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # hack to prevent admin promt
+    if 'syncdb' in sys.argv:
+        sys.argv.append('--noinput')
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PLEX.settings")
 
     from django.core.management import execute_from_command_line
